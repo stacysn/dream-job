e35122934ba8481e22a603174ebe1cd5c80cf700
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-09-30 14:33:24 -0700

    getting things to work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './navBar/Header'
+import Header from './navBar/Header';
+import CareerChoices from './careerChoices/careerchoices';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <Header />
+        <CareerChoices />
       </div>
     );
   }
