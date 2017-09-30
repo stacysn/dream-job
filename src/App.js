dfb4d062131da9bb4f72f1256b891609d16fa60a
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-09-30 13:59:02 -0700

    adding more circles

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './navBar/Header'
+import CareerChoices from './careerChoices/careerchoices'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
         <Header />
+        <CareerChoices />
       </div>
     );
   }
---
+++ src/careerChoices/careerchoices.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class CareerChoices extends Component {
+  render(){
+    return(
+      <header className="careermainbox">
+        <div className="career-image">
+        <div className="circle-text">
+          <div>Web Developer!</div>
+        </div>
+        <div className="circle-text">
+          <div>Web Designer!</div>
+        </div>
+        <div className="circle-text">
+          <div>User Experience!</div>
+        </div>
+        <div className="circle-text">
+          <div>Software Engineer!</div>
+        </div>
+        <div className="circle-text">
+          <div>Product Manager!</div>
+        </div>
+        <div className="circle-text">
+          <div>Digital Marketing!</div>
+        </div>
+        </div>
+
+      </header>
+    )
+  }
+}
+
+export default CareerChoices;
