89021939535ae162631ef782810c7453e4fcf599
%= GitConfig.user_name % <mary.e.howellstudios@gmail.com>
2017-09-30 16:00:14 -0700

    adding second page

---
+++ src/secondPage/secondPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SecondPage extends Component {
+  render(){
+    return(
+      <header>
+        <div className="computer-background"
+        </div> 
+      </header>
+    )
+  }
+}
