5b71c940d51d136d40faebff09a9831e3ff78576
stacysn <stacysuen5@gmail.com>
2017-09-30 13:00:00 -0700

    added hello world

---
+++ src/head/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render(){
+    return(
+      <div>
+        <b>"Hello World! "</b>
+      </div>
+    )
+  }
+}
+
+export default Header;
